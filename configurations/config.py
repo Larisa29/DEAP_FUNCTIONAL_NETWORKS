702cac503bb8aa58f5d2c11d80f5d3498ba44382
Larisa29 <larisa-maria.talmacel@student.tuiasi.ro>
2023-10-16 09:33:51 +0300

    config

---
+++ configurations/config.py
@@ -0,0 +1,4 @@
+import os
+
+#variabila necesara pentru a calcula corect calea absoluta catre directorul radacina al proiectului
+ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
